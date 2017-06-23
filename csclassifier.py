fc41300a8e6106ac252dc27dc9f925d2975389fc
Tyrus Cukavac <thc2125@columbia.edu>
2017-06-23 15:39:18 -0400

    Half-first pass at Jaech et. al's ANN model

---
+++ csclassifier.py
@@ -0,0 +1,59 @@
+from keras.models import Sequential, 
+from keras.layers import Conv1D, Dropout, MaxPooling1D, Dense, LSTM, 
+
+class csclassifier:
+    def __init__(self):
+        #TODO: Fix self.cdim=log(len(C))
+        self.cdim=10
+
+        # Numbers taken from Jaech et. al tuned hyperparameters
+        self.n_1 = 59
+        self.n_2 = 108
+        self.dropout_rate = .25
+
+        self.model = Sequential()
+        # Adding T_1 (1st CNN)
+        self.model.add(Conv1D(
+            input_shape=(None, self.cdim),
+            # 'filters' - the number of filters i.e. dimensionality of output
+            # Number taken from Jaech et. al tuned hyperparameters
+            filters=59
+            # 'kernel_size' - size of the window
+            kernel_size = 3
+            # 'padding' ensures the output will be the same length as input
+            padding='same'
+            activation = 'relu'))
+
+        # Adding the dropout
+        self.model.add(Dropout(rate=self.dropout_rate))
+
+        # Adding T_2 (2nd CNN)
+        self.model.add(Conv1D(
+            filters=self.n_2,
+            # (I don't know if making this a tuple will address multiple 
+            # filter windows...)
+            kernel_size = (3, 4, 5),
+            # TODO: T_2 uses padding so you end up with a vector for each
+            # character right?
+            padding='same'
+            activation='relu'))
+
+        # Add z (the residual network layer)
+        self.model.add(Dense
+
+        # Adding y (max-pooling across time)
+        self.model.add(MaxPooling1D(pool_size=1, padding='same'))
+
+
+        pass
+
+    def train(self):
+        """TKTK
+
+        Keyword arguments:
+        TKTK -- TKTK
+         """
+
+
+def transform_data():
+    pass
