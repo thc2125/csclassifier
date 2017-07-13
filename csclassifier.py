45f889ce44f23dc65b81a7b065db6c0a59047860
Tyrus Cukavac <thc2125@columbia.edu>
2017-07-13 09:32:33 -0700

    f1_score not working; trying sklearn's f1_score instead

--- csclassifier.py
+++ csclassifier.py
@@ -19,12 +19,15 @@ from math import log, ceil
 
 import numpy as np
 
+import keras.backend as K
+
 from keras.layers import Input, Embedding, Conv1D, Dropout, GlobalMaxPooling1D
 from keras.layers import Dense, Merge, TimeDistributed, Bidirectional, LSTM
 from keras.layers import add, concatenate
 from keras.models import Model
 from keras.utils import plot_model
 
+from sklearn import metrics
 from utils import Corpus
 
 class CSClassifier:
@@ -112,7 +115,8 @@ class CSClassifier:
         self.p = Dense(3, activation='softmax')(self.v)
         self.model = Model(inputs=self.inputs, outputs=self.p)
         # Note that 'adam' has a default learning rate of .001
-        self.model.compile(loss='categorical_crossentropy', optimizer='adam')
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', 
+            metrics=['categorical_accuracy', self.f1_score])
         plot_model(self.model, show_shapes=True)
 
     def classify_sentence(self, sentence):
@@ -130,16 +134,52 @@ class CSClassifier:
         TKTK -- TKTK
         """
         pass
+
+    def f1_score(self, y_true, y_pred):
+        """A Keras custom metric to evaluate f1 score
+
+        Keyword arguments:
+        y_true -- A tensor of gold labels
+        y_pred -- A tensor of predicted labels
+        """
+        # Create a tensor of values equal to the predicted category
+        y_true_cat = K.argmax(y_true, axis=-1)
+        y_pred_cat = K.argmax(y_pred, axis=-1)
+        cat_f1 = K.placeholder()
+        for i in range(3):
+            cat = K.constant(i, shape=y_true_cat.get_shape())
+            # Binary tensors representing all the labels equal to a 
+            # given category, i.e. get rid of any labels not for the 
+            # current cat
+            y_true_curr_cat = K.equal(y_true_cat, cat, K.floatx())
+            y_pred_curr_cat = K.equal(y_pred_cat, cat, K.floatx())
+            # Where do they match?
+            true_positives = K.equal(y_true_curr_cat,
+                                     y_pred_curr_cat, K.floatx())
+            # Where do they not match?
+            false_positives = K.not_equal(y_true_curr_cat,
+                                          y_pred_curr_cat, K.floatx())
+            # Sum the true positives and false positives
+            sum_tp = K.sum(true_positives)
+            sum_fp = K.sum(false_positives)
+            precision = sum_tp / (sum_tp + sum_fp)
+            recall = sum_tp / K.sum(y_true_curr_cat)
+            f1 = 2 * (precision * recall) / (precision + recall)
+            cat_f1 = K.concatenate([cat_f1, f1], axis=-1)
+        return cat_f1 
+        pass
 def main(corpus_filepath, epochs=20, batch_size=25, corpus_bin='corpus.bin'):
     # Ingest the corpus
     corpus = Corpus(corpus_filepath)
 
     #corpus.print_sentences_langs()
 
-    train_split = [ceil(9 * len(corpus.cs)/10)]
+    # train_split = [ceil(9 * len(corpus.cs)/10)]
+    train_split = [ceil(len(corpus.cs)/100), 2 * ceil(len(corpus.cs)/100)]
+
     #train_sentences, test_sentences = np.split(corpus.sentences, train_split)
-    train_sentences, test_sentences = np.split(corpus.sentences, train_split)
-    train_cs, test_cs = np.split(corpus.cs, train_split)
+    train_sentences, test_sentences, _ = np.split(corpus.sentences, train_split)
+    train_cs, test_cs, _ = np.split(corpus.cs, train_split)
     print(train_sentences.size)
     print(train_sentences.shape)
     print(train_cs.size)
@@ -152,6 +192,10 @@ def main(corpus_filepath, epochs=20, batch_size=25, corpus_bin='corpus.bin'):
     thistory = classifier.model.fit(x=train_sentences, y=train_cs,
             epochs=epochs, batch_size=batch_size, validation_split=.1)
     print(thistory)
+
+    # Evaluate the model
+    evaluation = classifier.model.evaluate(x=test_sentences, y=test_cs, batch_size=batch_size)
+    print(evaluation)
 
 
 if __name__ == '__main__':
