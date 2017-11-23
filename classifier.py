9350d75397a90494d7e9c77db72b4342732bb864
Tyrus Cukavac <thc2125@columbia.edu>
2017-11-22 19:43:33 -0800

    Turned csclassifier into a true subclass rather than a wrapper; classifier now holds both neural model and train/evaluation functions.

--- classifier.py
+++ classifier.py
@@ -1,32 +1,57 @@
+import json
+import time
+
 from math import log, ceil
+from pathlib import Path
 
 import keras.backend as K
+import keras.models
+import numpy as np
+
 from keras import optimizers
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.layers import Input, Embedding, Conv1D, Dropout, GlobalMaxPooling1D
 from keras.layers import Dense, Merge, TimeDistributed, Bidirectional, LSTM
 from keras.layers import add, concatenate
 from keras.models import Model
-#from keras.utils import plot_model
 
+import corpus_utils
+import utils
+
+DEFAULT_HYPER_PARAMETERS = {'n_1':59,
+                            'n_2':108, 
+                            'lstm_dim':23, 
+                            'dropout_rate':.25, 
+                            'kernel_size':3, 
+                            'loss':'categorical_crossentropy', 
+                            'optimizer':'adam', 
+                            'learning_rate':.001, 
+                            'decay':0}
 
 class Classifier:
+
     def __init__(self, 
-                 char_dicts, 
-                 label_dicts,
+                 chars, 
+                 labels,
                  maxsentlen, 
                  maxwordlen, 
+                 use_alphabets=False,
+                 hyper_parameters=DEFAULT_HYPER_PARAMETERS,
                  model=None):
 
-        self.idx2char, self.char2idx = char_dicts
-        self.idx2label, self.label2idx = label_dicts
-        num_labels = len(self.idx2label)
-
+        self.idx2char, self.char2idx = utils.get_char_dicts(chars, use_alphabets)
+        self.idx2label, self.label2idx = utils.get_label_dicts(labels)
+        
         self.maxsentlen = maxsentlen
         self.maxwordlen = maxwordlen
+
+        self.use_alphabets = use_alphabets
+
         if model:
-            elf.classifier.model = load_model(model)
+            self.model = self._load_model(model)
 
-
+        else:
+            self._generate_model(**hyper_parameters)
 
     def _generate_model(self, 
                         n_1=59,
@@ -39,7 +64,9 @@ class Classifier:
                         learning_rate=.001, 
                         decay=0):
         # Create the model
-        self.C = char2idx.values()
+        self.num_labels = len(self.idx2label)
+
+        self.C = self.char2idx.values()
 
         self.Cdim = ceil(log(len(self.C), 2))
 
@@ -66,71 +93,184 @@ class Classifier:
         # This could be created during corpus ingestion and just be another value.
         # Maybe pop this input/merge right after the embedding layer and before 
         # the CNN.
-        self.inputs = Input(shape=(maxsentlen, maxwordlen,))
+        inputs = Input(shape=(self.maxsentlen, self.maxwordlen,))
 
         # Set up the embeddings
         # TODO: Add the masking layer for the padded value
-        self.embeddings = TimeDistributed(Embedding(len(self.C),
-            self.Cdim))(self.inputs)
+        embeddings = TimeDistributed(Embedding(len(self.C),
+            self.Cdim))(inputs)
 
         # Make T_1 (1st CNN)
-        self.T_1 = TimeDistributed(Conv1D(
+        T_1 = TimeDistributed(Conv1D(
             # input shape can be deduced
             # 'filters' - the number of filters i.e. dimensionality of output
-            filters=self.n_1,
+            filters=n_1,
             # 'kernel_size' - size of the window
-            kernel_size=self.kernel_size,
+            kernel_size=kernel_size,
             # 'padding' ensures the output will be the same length as input
             padding='same',
-            activation = 'relu'))(self.embeddings)
+            activation = 'relu'))(embeddings)
 
 
 
         # Adding the dropout
-        self.T_1_dropout = Dropout(rate=self.dropout_rate)(self.T_1)
+        T_1_dropout = Dropout(rate=dropout_rate)(T_1)
 
         # Adding T_2 (2nd CNN)
-        self.T_2_a = TimeDistributed(Conv1D(
-            filters=self.n_2,
+        T_2_a = TimeDistributed(Conv1D(
+            filters=n_2,
             kernel_size = (3),
             padding='valid',
-            activation='relu'))(self.T_1_dropout)
+            activation='relu'))(T_1_dropout)
 
-        self.T_2_b = TimeDistributed(Conv1D(
-            filters=self.n_2,
+        T_2_b = TimeDistributed(Conv1D(
+            filters=n_2,
             kernel_size = (4),
             padding='valid',
-            activation='relu'))(self.T_1_dropout)
+            activation='relu'))(T_1_dropout)
 
-        self.T_2_c = TimeDistributed(Conv1D(
-            filters=self.n_2,
+        T_2_c = TimeDistributed(Conv1D(
+            filters=n_2,
             kernel_size = (5),
             # TODO: T_2 uses padding so you end up with a vector for each
             # character right?
             padding='valid',
-            activation='relu'))(self.T_1_dropout)
-
-
+            activation='relu'))(T_1_dropout)
 
         # Adding y (max-pooling across time)
-        self.y_a = TimeDistributed(GlobalMaxPooling1D())(self.T_2_a)
-        self.y_b = TimeDistributed(GlobalMaxPooling1D())(self.T_2_b)
-        self.y_c = TimeDistributed(GlobalMaxPooling1D())(self.T_2_c)
-        self.y = concatenate([self.y_a, self.y_b, self.y_c])
+        y_a = TimeDistributed(GlobalMaxPooling1D())(T_2_a)
+        y_b = TimeDistributed(GlobalMaxPooling1D())(T_2_b)
+        y_c = TimeDistributed(GlobalMaxPooling1D())(T_2_c)
+        y = concatenate([y_a, y_b, y_c])
 
         # Add f_r(y) 
-        self.f_r_y = Dense(3 * self.n_2, activation='relu')(self.y)
-        self.z = add([self.y, self.f_r_y])
+        f_r_y = Dense(3 * n_2, activation='relu')(y)
+        z = add([y, f_r_y])
 
         # The next phase is dealing with all the words in the sentence.     
         # TODO: Are default lstm activation functions okay?
-        self.v = Bidirectional(LSTM(self.lstm_dim,
-            return_sequences=True, dropout=self.dropout_rate))(self.z)
-        self.p = Dense(num_labels, activation='softmax')(self.v)
-        self.model = Model(inputs=self.inputs, outputs=self.p)
+        v = Bidirectional(LSTM(lstm_dim,
+            return_sequences=True, dropout=dropout_rate))(z)
+        p = Dense(self.num_labels, activation='softmax')(v)
+        self.model = Model(inputs=inputs, outputs=p)
         # Note that 'adam' has a default learning rate of .001
-        self.model.compile(loss=self.loss, 
-            optimizer=self.optimizer, 
+        self.model.compile(loss=loss, 
+            optimizer=optimizer, 
             metrics=['categorical_accuracy'], sample_weight_mode='temporal')
-        #plot_model(self.model, show_shapes=True)
 
+        self.trained_epochs = 0
+
+    def _load_model(self, model_path):
+        self.model = keras.models.load_model(model_path)
+
+    def plot_model(self):
+        keras.utils.plot_model(self.model, show_shapes=True)
+
+    def train_model(self, 
+                    train_corpus, 
+                    epochs,
+                    batch_size,
+                    patience,
+                    output_dirpath='.'):
+        if (train_corpus.maxsentlen > self.maxsentlen or 
+            train_corpus.maxwordlen > self.maxwordlen):
+            raise Exception("'train_corpus' has greater maxsentlen or maxwordlen")
+
+        raw_train_sentences = self._get_corpus_sentences(train_corpus)
+        raw_train_labels = self._get_corpus_labels(train_corpus)
+
+        train_sentences, train_labels = corpus_utils.np_idx_conversion(
+                                            raw_train_sentences, 
+                                            raw_train_labels, 
+                                            self.maxsentlen,
+                                            self.maxwordlen,
+                                            self.char2idx,
+                                            self.idx2label,
+                                            train_corpus.char_count)
+
+        # Train the model
+        # Create a folder to store checkpoints if one does not exist
+        checkpoints_dirpath = Path(output_dirpath / 'checkpoints')
+        if not checkpoints_dirpath.exists():
+            checkpoints_dirpath.mkdir()
+        checkpoint = ModelCheckpoint(
+                         filepath=str(checkpoints_dirpath/('cp_classifier_model_'
+                                      + "_"
+                                      + '.{epoch:02d}--'
+                                      + '{val_loss:.2f}.hdf5')),
+                         monitor='val_loss', 
+                         mode='min')
+        stop_early = EarlyStopping(monitor='val_categorical_accuracy',
+                                   patience=patience)
+        self.history = self.model.fit(x=train_sentences, 
+                                      y=train_labels,
+                                      epochs=epochs, 
+                                      batch_size=batch_size, 
+                                      validation_split=.1,
+                                      #sample_weight=train_labels_weights, 
+                                      callbacks=[checkpoint, 
+                                      stop_early])
+        self.trained_time = time.time()
+        self.trained_epochs += len(self.history.epoch)
+        
+    def save_model(self, output_dirpath=Path('.')):
+        # Save the model
+        model_path = (output_dirpath
+                            / ('classifier_model_' 
+                               + str(self.trained_time)
+                               + '_' 
+                               + '.h5'))
+        self.model.save(str(model_path))
+
+        # Save the history
+        model_history_path = (model_path.parent 
+                             / (model_path.stem + '_history.json'))
+
+        with (model_history_path).open('w') as history_file:
+            json.dump(self.history.history, 
+                      history_file, 
+                      indent=4, 
+                      sort_keys=True)
+
+        return model_path, model_history_path 
+
+    def evaluate_model(self, test_corpus):
+        if (test_corpus.maxsentlen > self.maxsentlen or 
+            test_corpus.maxwordlen > self.maxwordlen):
+            raise Exception("'train_corpus' has greater maxsentlen or maxwordlen")
+
+        raw_test_sentences = self._get_corpus_sentences(test_corpus)
+        raw_test_labels = self._get_corpus_labels(test_corpus)
+
+        test_sentences, test_labels = corpus_utils.np_idx_conversion(
+                                            raw_test_sentences, 
+                                            raw_test_labels, 
+                                            self.maxsentlen,
+                                            self.maxwordlen,
+                                            self.char2idx,
+                                            self.idx2label)
+
+        print("Testing on sentences of shape: " + str(test_sentences.shape))
+        pred_labels = self.model.predict(x=test_sentences)
+
+        # Transform labels to represent category index
+        test_cat_labels = np.argmax(test_labels, axis=2)
+        pred_cat_labels = np.argmax(pred_labels, axis=2)
+
+        metrics = {}
+        metrics['word'] = (utils.compute_accuracy_metrics(
+            test_cat_labels, pred_cat_labels, self.label2idx))
+
+        test_sent_cat_labels = np.argmax(test_labels, axis=2)
+        pred_sent_cat_labels = np.argmax(pred_labels, axis=2)
+
+        metrics['sentence'] = (utils.compute_accuracy_metrics(
+            test_sent_cat_labels, pred_sent_cat_labels, self.label2idx))
+
+        return metrics 
+
+    def _get_corpus_labels(self, corpus):
+        return corpus.slabels
+
+    def _get_corpus_sentences(self, corpus):
+        return corpus.sentences
--- csclassifier.py
+++ csclassifier.py
@@ -9,140 +9,29 @@
 # Model design as described by Jaech et. al. in
 # "A Neural Model for Language Identification in Code-Switched Tweets"
 
-
-import argparse
-import csv
-import random
-import pickle
-import os
-
-from math import ceil
-from pathlib import Path, PurePath
-
-import numpy as np
-from keras.models import load_model
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-
-import utils
-
-from utils import Corpus, Corpus_Aaron
+import classifier
 from classifier import Classifier
 
-class CSClassifier():
-    def __init__(self, 
-                 maxsentlen, 
-                 maxwordlen, 
-                 label2idx, 
-                 idx2label, 
-                 char2idx, 
-                 idx2char, 
-                 epochs, 
-                 batch_size, 
-                 patience, 
-                 use_alphabets,
-                 train_langs, 
-                 test_langs):
-        self.maxsentlen = maxsentlen
-        self.maxwordlen = maxwordlen
-        self.label2idx = label2idx
-        self.idx2label = idx2label
-        self.char2idx = char2idx
-        self.idx2char = idx2char
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.patience = patience
-        self.use_alphabets = use_alphabets
-        self.train_langs = train_langs
-        self.identifier = hash(tuple(train_langs))
-        self.test_langs = test_langs
+class CSClassifier(Classifier):
 
-    def generate_model(self, train_corpus, model=None, output_dirpath=PurePath('.')):
-        #train_split = [ceil(9 * len(corpus.sentences)/10)]
-        #train_split = [ceil(len(corpus.sentences)/100), 2 * ceil(len(corpus.sentences)/100)]
-        if (train_corpus.maxsentlen > self.maxsentlen or 
-            train_corpus.maxwordlen > self.maxwordlen):
-            raise Exception("'train_corpus' has greater maxsentlen or maxwordlen")
+    labels = ['no_cs', 'cs']
 
-        self.train_corpus = train_corpus
+    def __init__(self,
+                 chars,
+                 maxsentlen,
+                 maxwordlen,
+                 use_alphabets=False,
+                 hyper_parameters=classifier.DEFAULT_HYPER_PARAMETERS,
+                 model=None):
 
-        train_sentences, train_labels, train_labels_weights = (
-            utils.np_idx_conversion(
-                train_corpus, 
-                self.maxsentlen,
-                self.maxwordlen,
-                self.char2idx,
-                self.use_alphabets,
-                self.label2idx))
+        Classifier.__init__(self, 
+                            chars, 
+                            self.labels,
+                            maxsentlen, 
+                            maxwordlen, 
+                            use_alphabets,
+                            hyper_parameters,
+                            model)
 
-        num_labels = len(self.label2idx)
-
-        # Build the model
-        self.classifier = Classifier(self.char2idx, 
-                                     self.maxsentlen, 
-                                     self.maxwordlen, 
-                                     num_labels)
-
-        if model != None:
-            # Load the model
-            self.classifier.model = load_model(model)
-        else:
-            # Train the model
-            alph = 'alph' if self.train_corpus.use_alphabets else ''
-            # Create a folder to store checkpoints if one does not exist
-            checkpoints_dirpath = Path(output_dirpath, 'checkpoints')
-            if not checkpoints_dirpath.exists():
-                checkpoints_dirpath.mkdir()
-            checkpoint = ModelCheckpoint(
-                    filepath=str(checkpoints_dirpath/('cp_'
-                                 + self.identifier
-                                 + "_"
-                                 + alph
-                                 + '.{epoch:02d}--'
-                                 + '{val_loss:.2f}.hdf5')),
-                    monitor='val_loss', 
-                    mode='min')
-            stop_early = EarlyStopping(
-                    monitor='val_categorical_accuracy',
-                    patience=self.patience)
-            self.history = self.classifier.model.fit(x=train_sentences, y=train_labels,
-                epochs=self.epochs, batch_size=self.batch_size, validation_split=.1,
-                sample_weight=train_labels_weights, 
-                callbacks=[checkpoint, stop_early])
-            # Save the model
-            self.classifier.model.save(str(output_dirpath / 
-                ('cs_classifier_model_' + self.identifier + '_' + alph + '.h5')))
-            self.trained_epochs = len(self.history.epoch)
-        return self.classifier
-
-    def evaluate_model(self, test_corpus):
-        # Evaluate the model
-        #evaluation = classifier.model.evaluate(x=test_sentences, y=test_cs, batch_size=batch_size)
-        #print(evaluation)
-        #TODO: remove the following two commented out lines
-        #maxsentlen = train_corpus.maxsentlen
-        #maxwordlen = train_corpus.maxwordlen
-        self.test_corpus = test_corpus
-        test_sentences, test_labels, test_labels_weights = self.test_corpus.np_idx_conversion(self.maxsentlen,
-            self.maxwordlen)
-
-        print("Testing on sentences of shape: " + str(test_sentences.shape))
-        pred_labels = self.classifier.model.predict(x=test_sentences)
-
-        # Transform labels to represent category index
-        test_cat_labels = np.argmax(test_labels, axis=2)
-        pred_cat_labels = np.argmax(pred_labels, axis=2)
-
-        metrics = {}
-
-        metrics['word'] = (utils.compute_accuracy_metrics(
-            test_cat_labels, pred_cat_labels, self.label2idx))
-
-        # Reduce gold labels and predicted labels to sentence level
-        # cs identification
-        test_cat_labels_sent = np.maximum.reduce(test_cat_labels,axis=1, keepdims=True)
-        pred_cat_labels_sent = np.maximum.reduce(pred_cat_labels,axis=1, keepdims=True)
-
-        metrics['sentence'] = (utils.compute_accuracy_metrics(
-            test_cat_labels_sent, pred_cat_labels_sent, self.label2idx))
-
-        return metrics
+    def _get_corpus_labels(self, corpus):
+        return corpus.scslabels
