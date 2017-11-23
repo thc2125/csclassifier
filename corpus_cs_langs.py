0e8c2bfead66fcf9bdbd1f046d3d958c43173eea
Tyrus Cukavac <thc2125@columbia.edu>
2017-11-22 19:45:35 -0800

    Decoupled index functions from class/object. Changed nomenclature/functions to match those of the revised corpus class.

--- corpus_cs_langs.py
+++ corpus_cs_langs.py
@@ -1,57 +1,58 @@
+import math
+
+from collections import Counter
+
 from corpus import Corpus
 
-class Corpus_CS_Langs(Corpus):
+class CorpusCSLangs(Corpus):
     def __init__(self):
 
         Corpus.__init__(self)
 
         self.scslabels = []
-        self.cslabel_frequency = Counter()
+        self.cslabel_count = Counter()
+        self.cstype_count = Counter()
 
-    def _init_data(self):
-        self.switch_count = 0
-        self.switch_label_counts = defaultdict(int)
-        self.multilingual_sentence_count = 0
-        self.cs_labels = []
-        Corpus._init_data(self)
+        self.lang_pair_count = Counter()
 
-    def ingest_token(self, token_entry, corpus_filename):
-        sidx = Corpus.ingest_token(self, token_entry, corpus_filename)
-        if(not sidx):
+        self.multilingual_sentences = set()
+
+    def _ingest_token(self, token_entry):
+        sidx = Corpus._ingest_token(self, token_entry)
+        if(math.isnan(sidx)):
             return None
+
+        lang_pair = token_entry['lang_pair']
+        self._add_lang_pair(lang_pair)
+
         cslabel = token_entry['cs_label']
         self._add_cslabel(cslabel, sidx)
 
+        cstype = token_entry['cs_type']
+        self._add_cstype(cstype, sidx)
+
+    def _add_sentence(self, sname):
+        Corpus._add_sentence(self, sname)
+        self.scslabels.append([])
+
+    def _add_lang_pair(self, lang_pair):
+        self.lang_pair_count.update([lang_pair])
+        return lang_pair
+
     def _add_cslabel(self, cslabel, sidx):
         self.scslabels[sidx].append(cslabel)
-        self.cslabel_frequency.update([cslabel])
+        self.cslabel_count.update([cslabel])
+        if cslabel == 'cs':
+            self.multilingual_sentences.add(sidx)
         return cslabel
 
+    def _add_cstype(self, cstype, sidx):
+        if cstype != '<na>':
+            self.cstype_count.update([cstype])
+        return cstype
+
     def get_cslabels(self):
-        return list(self.cslabel_frequency.keys())
+        return list(self.cslabel_count.keys())
 
-    def np_idx_conversion(self, maxsentlen, maxwordlen):
-        list_sentences, list_labels, list_labels_weights = (
-            Corpus.np_idx_conversion(self, maxsentlen, maxwordlen)
-
-        list_cs_labels, list_cs_labels_weights = self.cs_labels_idx_conversion(
-            maxsentlen, maxwordlen)
-
-        return (list_sentences, list_labels, list_labels_weights, 
-            list_cs_labels, list_cs_labels_weights)
-    
-    def cs_labels_idx_conversion(self, maxsentlen, maxwordlen):
-        list_cat_cs_labels = ([[self.cs_label2idx[label] for label in sentlabels] 
-                + [0] * (maxsentlen-len(sentlabels)) 
-            for sentlabels in self.cs_labels])
-        # Make labels one-hot
-        list_cs_labels = ([to_categorical(sentlabels, 
-                num_classes=len(self.cs_label2idx)) 
-            for sentlabels in list_cat_cs_labels])
-
-        list_cs_labels_weights = ([[(1 if label != 0 else 0) for label in list_slabels] 
-            for list_slabels in list_cat_cs_labels])
-
-        return list_cs_labels, list_cs_labels_weights
-
-
+    def get_cstypes(self):
+        return list(self.cstype_count.keys())
